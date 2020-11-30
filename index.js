9092413ea7ffe183b5e17deb8b605ca0b9d5108f
Jin Cheng <jincheng411@gmail.com>
2020-11-29 22:47:11 -0500

    added delete method

--- index.js
+++ index.js
@@ -58,6 +58,12 @@ app.put('/products/:id', async (req, res) => {
     res.redirect(`/products/${product._id}`);
 })
 
+app.delete('/products/:id', async (req, res) => {
+    const {id} = req.params;
+    await Product.findByIdAndDelete(id);
+    res.redirect('/products')
+})
+
 app.listen(3000,()=>{
     console.log('port 3000')
 })
